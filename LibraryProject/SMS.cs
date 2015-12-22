20c1ad311b21755ee65c9cd7dfa28084cdf73ce8
Emre Arslan <yemrearslann@gmail.com>
2015-12-22 13:29:16 +0200

    Mail ve SMS modülleri eklendi

---
+++ LibraryProject/SMS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+namespace Mail_Module
+{
+    public string Name { get; set; }
+    public partial class SMS : Form {
+        private const string URL = "https://api-gw.turkcell.com.tr/api/v1/sms";
+        private string urlParameters = "8403cb98-fe34-48e8-88b8-79aa3f2081bd";
+
+        private void SMS_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    static void SendSMS(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(URL);
+
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("api_key:8403cb98-fe34-48e8-88b8-79aa3f2081bd"));
+
+    }
+   
+
+
+}
---
+++ LibraryProject/SMS.designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Mail_Module
+{
+    partial class SMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Text:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(117, 85);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // SMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 275);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "SMS";
+            this.Text = "SMS";
+            this.Load += new System.EventHandler(this.SMS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
