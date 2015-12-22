abe565aea02458246c8b1b526cab9209dcf1ffe5
Emre Arslan <yemrearslann@gmail.com>
2015-12-22 13:33:07 +0200

    Mail modülü

---
+++ LibraryProject/Mail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mail_Module
+{
+    public partial class Mail : Form
+    {
+                public static string USERNAME = "80ddbdb6-aa41-42b6-90ca-959890158ce3";
+                public static string API_KEY = "80ddbdb6-aa41-42b6-90ca-959890158ce3";
+
+
+        public static string SendEmail(string to, string subject, string bodyText, string from, string fromName)
+        {
+
+            WebClient client = new WebClient();
+            NameValueCollection values = new NameValueCollection();
+            values.Add("username", USERNAME);
+            values.Add("api_key", API_KEY);
+            values.Add("from", from);
+            values.Add("from_name", fromName);
+            values.Add("subject", subject);
+            if (bodyText != null)
+                values.Add("body_text", bodyText);
+            values.Add("to", to);
+
+            byte[] response = client.UploadValues("https://api.elasticemail.com/mailer/send", values);
+            return Encoding.UTF8.GetString(response);
+        }
+
+
+        public Mail()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void buttonMailSend_Click(object sender, EventArgs e)
+        {
+            string from = "toroslibrary@mail.com";
+            string fromName = "Toros Kütüphane";
+            string to = textBox1.Text;
+            string subject = "Ödünç Kitap";
+            string bodyText = richTextBox1.Text;
+            string result = SendEmail(to, subject, bodyText, from, fromName);
+            MessageBox.Show(result);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SMS SMS = new SMS();
+            SMS.Show();
+        }
+
+        private void Mail_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LibraryProject/Mail.designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Mail_Module
+{
+    partial class Mail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMailSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMailSend
+            // 
+            this.buttonMailSend.Location = new System.Drawing.Point(326, 230);
+            this.buttonMailSend.Name = "buttonMailSend";
+            this.buttonMailSend.Size = new System.Drawing.Size(86, 37);
+            this.buttonMailSend.TabIndex = 0;
+            this.buttonMailSend.Text = "Gönder";
+            this.buttonMailSend.UseVisualStyleBackColor = true;
+            this.buttonMailSend.Click += new System.EventHandler(this.buttonMailSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(137, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(275, 127);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mesaj:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SMS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Mail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 289);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonMailSend);
+            this.Name = "Mail";
+            this.Text = "Mail";
+            this.Load += new System.EventHandler(this.Mail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMailSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
